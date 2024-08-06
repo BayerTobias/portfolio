7ff3b7c94587b02f1a2e7520cc2493213ab40659
BayerTobias <tbayer2@gmx.de>
2024-08-06 19:52:32 +0200

    text and link changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -18,10 +18,10 @@ export class PortfolioComponent {
   ];
 
   descriptions: string[] = [
-    'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-    'A side-scrolling space shooter game based on an object-oriented approach. Navigate your spacecraft through the vast expanse of outer space, confronting space pirates and ultimately vanquishing their leader at the end of your galactic journey.',
+    'Join is a task manager inspired by the Kanban system. The frontend is built with Angular V17 and TypeScript, focusing on the new Angular Signals to ensure a reactive and high-performance user interface. With drag-and-drop functionality, you can easily create and organize tasks, assigning them to users and categories. The backend consists of a robust REST API implemented with Django and the Django REST Framework in Python, providing scalable and efficient data management.',
+    'Space-Shooter is a side-scrolling space shooter game developed with JavaScript, HTML, and CSS, based on an object-oriented approach. Object-oriented programming allows for a modular and extendable game structure, ensuring a clear separation of game components and easy code maintenance.',
     'A sleek chat messenger built with Angular, powered by Firebase technologies. Drawing design inspiration from Slack, its intuitive user interface provides a familiar and user-friendly experience. Firebase Authentication ensures secure logins, while Firestore enables efficient data storage and retrieval.',
-    'netflix inspired... etc etc django python toll toll toll',
+    'Videoflix is a Netflix-inspired streaming portal with an Angular frontend. The backend uses the Django REST Framework to provide a scalable API. Background tasks are handled with Django RQ, while Redis is used for both the database and caching. FFmpeg is used for video conversion and optimization, enabling efficient management and delivery of video content.',
   ];
 
   projectLinks: string[] = [
@@ -34,8 +34,8 @@ export class PortfolioComponent {
   githubLinks: string[] = [
     'https://github.com/BayerTobias/join',
     'https://github.com/BayerTobias/SpaceGame',
-    'https://github.com/BayerTobias/DABubble', // ändern für dabubble
-    'https://github.com/BayerTobias/videoflix-frontend-angular-17',
+    'https://github.com/BayerTobias/DABubble',
+    'https://github.com/BayerTobias/videoflix-backend',
   ];
 
   images: string[] = [
